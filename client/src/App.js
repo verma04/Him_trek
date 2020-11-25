bdd77bfdbea7472f03fab5ddbfdb572976027341
Pankaj <pankaj.k@amakein.com>
2020-11-25 15:29:33 +0530

    upadtes

--- client/src/App.js
+++ client/src/App.js
@@ -7,8 +7,8 @@ import { setCurrentUser, logoutUser } from "./actions/authActions";
 import { Provider } from "react-redux";
 import store from "./store";
 
-import Navbar from "./components/layout/Navbar";
-import Landing from "./components/layout/Landing";
+import Navbar from "./components/layout/Navbar/Navbar";
+import Landing from "./components/layout/Landing/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import PrivateRoute from "./components/private-route/PrivateRoute";
